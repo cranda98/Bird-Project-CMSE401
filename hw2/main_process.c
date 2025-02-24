7504eb461df7b04dcfb64b9b5eb07f99c474c7c5
Lauryn Crandall <cranda98@msu.edu>
2025-02-23 23:06:32 -0500

    Update main_process.c

--- hw2/main_process.c
+++ hw2/main_process.c
@@ -7,11 +7,10 @@
 #include <time.h>
 #include "png_util.h"
 
-#define min(X,Y) ((X) < (Y) ? (X) : (Y))
-#define max(X,Y) ((X) > (Y) ? (X) : (Y))
+#define min(X, Y) ((X) < (Y) ? (X) : (Y))
+#define max(X, Y) ((X) > (Y) ? (X) : (Y))
 
-void abort_(const char * s, ...)
-{
+void abort_(const char *s, ...) {
     va_list args;
     va_start(args, s);
     vfprintf(stderr, s, args);
@@ -20,145 +19,121 @@ void abort_(const char * s, ...)
     abort();
 }
 
-char ** process_img(char ** img, char ** output, image_size_t sz, int halfwindow, double thresh)
-{
-    // Start timing for the entire process_img function
-    clock_t start_total = clock();
-
-    // Start timing for the Average Filter
-    clock_t start_avg = clock();
-
-    // Average Filter (Reordered Loops for Cache Optimization)
-    for(int r=0;r<sz.height;r++)  // Outer loop: rows
-        for(int c=0;c<sz.width;c++)  // Inner loop: columns
-        {
+void apply_average_filter(char **img, char **output, image_size_t sz, int halfwindow) {
+    for (int r = 0; r < sz.height; r++) {
+        for (int c = 0; c < sz.width; c++) {
             double count = 0;
             double tot = 0;
-            for(int rw=max(0,r-halfwindow); rw<min(sz.height,r+halfwindow+1); rw++)  // Window rows
-                for(int cw=max(0,c-halfwindow); cw<min(sz.width,c+halfwindow+1); cw++)  // Window columns
-                {
+            for (int rw = max(0, r - halfwindow); rw < min(sz.height, r + halfwindow + 1); rw++) {
+                for (int cw = max(0, c - halfwindow); cw < min(sz.width, c + halfwindow + 1); cw++) {
                     count++;
-                    tot += (double) img[rw][cw];
+                    tot += (double)img[rw][cw];
                 }
-            output[r][c] = (int) (tot/count);
+            }
+            output[r][c] = (int)(tot / count);
         }
+    }
+}
 
-    // End timing for the Average Filter
-    clock_t end_avg = clock();
-    double elapsed_avg = (double)(end_avg - start_avg) / CLOCKS_PER_SEC;
-    printf("Average Filter Execution Time: %.4f seconds\n", elapsed_avg);
+void apply_sobel_filter(char **img, double **gradient, image_size_t sz) {
+    double xfilter[3][3] = {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
+    double yfilter[3][3] = {{1, 2, 1}, {0, 0, 0}, {-1, -2, -1}};
 
-    // Start timing for the Sobel Filters and Gradient Calculation
-    clock_t start_gradient = clock();
-
-    // Sobel Filters (Precomputed)
-    double xfilter[3][3] = {
-        {-1, 0, 1},
-        {-2, 0, 2},
-        {-1, 0, 1}
-    };
-    double yfilter[3][3] = {
-        {1, 2, 1},
-        {0, 0, 0},
-        {-1, -2, -1}
-    };
-
-    // Allocate memory for gradient image
-    double * gradient = (double *) malloc(sz.width*sz.height*sizeof(double));
-    double ** g_img = malloc(sz.height * sizeof(double*));
-    for (int r=0; r<sz.height; r++)
-        g_img[r] = &gradient[r*sz.width];
-
-    // Gradient filter (Reordered Loops for Cache Optimization)
-    for(int r=1;r<sz.height-1;r++)  // Outer loop: rows
-        for(int c=1;c<sz.width-1;c++)  // Inner loop: columns
-        {
+    for (int r = 1; r < sz.height - 1; r++) {
+        for (int c = 1; c < sz.width - 1; c++) {
             double Gx = 0;
             double Gy = 0;
-            for(int rw=0; rw<3; rw++)  // Filter rows
-                for(int cw=0; cw<3; cw++)  // Filter columns
-                {
-                    Gx +=  ((double) output[r+rw-1][c+cw-1])*xfilter[rw][cw];
-                    Gy +=  ((double) output[r+rw-1][c+cw-1])*yfilter[rw][cw];
+            for (int rw = 0; rw < 3; rw++) {
+                for (int cw = 0; cw < 3; cw++) {
+                    Gx += ((double)img[r + rw - 1][c + cw - 1]) * xfilter[rw][cw];
+                    Gy += ((double)img[r + rw - 1][c + cw - 1]) * yfilter[rw][cw];
                 }
-            g_img[r][c] = sqrt(Gx*Gx+Gy*Gy);
+            }
+            gradient[r][c] = sqrt(Gx * Gx + Gy * Gy);
         }
+    }
+}
 
-    // End timing for the Gradient Calculation
+void apply_threshold(double **gradient, char **output, image_size_t sz, double thresh) {
+    for (int r = 0; r < sz.height; r++) {
+        for (int c = 0; c < sz.width; c++) {
+            output[r][c] = (gradient[r][c] > thresh) ? 255 : 0;
+        }
+    }
+}
+
+char **process_img(char **img, char **output, image_size_t sz, int halfwindow, double thresh) {
+    clock_t start_total = clock();
+
+    // Apply average filter
+    clock_t start_avg = clock();
+    apply_average_filter(img, output, sz, halfwindow);
+    clock_t end_avg = clock();
+    printf("Average Filter Execution Time: %.4f seconds\n", (double)(end_avg - start_avg) / CLOCKS_PER_SEC);
+
+    // Apply Sobel filter and calculate gradient
+    clock_t start_gradient = clock();
+    double *gradient = (double *)malloc(sz.width * sz.height * sizeof(double));
+    double **g_img = malloc(sz.height * sizeof(double *));
+    for (int r = 0; r < sz.height; r++) {
+        g_img[r] = &gradient[r * sz.width];
+    }
+    apply_sobel_filter(output, g_img, sz);
     clock_t end_gradient = clock();
-    double elapsed_gradient = (double)(end_gradient - start_gradient) / CLOCKS_PER_SEC;
-    printf("Gradient Calculation Execution Time: %.4f seconds\n", elapsed_gradient);
+    printf("Gradient Calculation Execution Time: %.4f seconds\n", (double)(end_gradient - start_gradient) / CLOCKS_PER_SEC);
 
-    // Start timing for Thresholding
+    // Apply thresholding
     clock_t start_thresh = clock();
-
-    // Thresholding (Reordered Loops for Cache Optimization)
-    for(int r=0;r<sz.height;r++)  // Outer loop: rows
-        for(int c=0;c<sz.width;c++)  // Inner loop: columns
-            if (g_img[r][c] > thresh)
-                output[r][c] = 255;
-            else
-                output[r][c] = 0;
-
-    // End timing for Thresholding
+    apply_threshold(g_img, output, sz, thresh);
     clock_t end_thresh = clock();
-    double elapsed_thresh = (double)(end_thresh - start_thresh) / CLOCKS_PER_SEC;
-    printf("Thresholding Execution Time: %.4f seconds\n", elapsed_thresh);
+    printf("Thresholding Execution Time: %.4f seconds\n", (double)(end_thresh - start_thresh) / CLOCKS_PER_SEC);
 
-    // End timing for the entire process_img function
-    clock_t end_total = clock();
-    double elapsed_total = (double)(end_total - start_total) / CLOCKS_PER_SEC;
-    printf("Total Execution Time: %.4f seconds\n", elapsed_total);
-
-    // Free allocated memory
+    // Free memory
     free(gradient);
     free(g_img);
+
+    clock_t end_total = clock();
+    printf("Total Execution Time: %.4f seconds\n", (double)(end_total - start_total) / CLOCKS_PER_SEC);
 
     return output;
 }
 
-int main(int argc, char **argv)
-{
-    // Code currently does not support more than one channel (i.e. grayscale only)
-    int channels=1; 
+int main(int argc, char **argv) {
+    int channels = 1;
     double thresh = 50;
     int halfwindow = 3;
 
-    // Ensure at least two input arguments
-    if (argc < 3 )
+    if (argc < 3) {
         abort_("Usage: process <file_in> <file_out> <halfwindow=3> <threshold=50>");
+    }
 
-    // Set optional window argument
-    if (argc > 3 )
+    if (argc > 3) {
         halfwindow = atoi(argv[3]);
+    }
 
-    // Set optional threshold argument
-    if (argc > 4 )
-        thresh = (double) atoi(argv[4]);
+    if (argc > 4) {
+        thresh = (double)atoi(argv[4]);
+    }
 
-    // Allocate memory for images
     image_size_t sz = get_image_size(argv[1]);
-    char * s_img = (char *) malloc(sz.width*sz.height*channels*sizeof(char));
-    char * o_img = (char *) malloc(sz.width*sz.height*channels*sizeof(char));
+    char *s_img = (char *)malloc(sz.width * sz.height * channels * sizeof(char));
+    char *o_img = (char *)malloc(sz.width * sz.height * channels * sizeof(char));
 
-    // Read in serial 1D memory
-    read_png_file(argv[1],s_img,sz);
+    read_png_file(argv[1], (unsigned char *)s_img, sz);
 
-    // Make 2D pointer arrays from 1D image arrays
-    char **img = malloc(sz.height * sizeof(char*));
-    for (int r=0; r<sz.height; r++)
-        img[r] = &s_img[r*sz.width];
-    char **output = malloc(sz.height * sizeof(char*));
-    for (int r=0; r<sz.height; r++)
-        output[r] = &o_img[r*sz.width];
+    char **img = malloc(sz.height * sizeof(char *));
+    for (int r = 0; r < sz.height; r++) {
+        img[r] = &s_img[r * sz.width];
+    }
+    char **output = malloc(sz.height * sizeof(char *));
+    for (int r = 0; r < sz.height; r++) {
+        output[r] = &o_img[r * sz.width];
+    }
 
-    // Run the main image processing function
-    process_img(img,output,sz,halfwindow,thresh);
+    process_img(img, output, sz, halfwindow, thresh);
 
-    // Write out output image using 1D serial pointer
-    write_png_file(argv[2],o_img,sz);
+    write_png_file(argv[2], (unsigned char *)o_img, sz);
 
-    // Free allocated memory
     free(s_img);
     free(o_img);
     free(img);
